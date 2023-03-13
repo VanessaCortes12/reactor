2e9adc1cbefff329da7714c279e03a937deb962f
VanessaCortes12 <carly_vane124@hotmail.com>
2023-03-12 18:54:32 -0600

    Se agregó programa de entrada de datos

---
+++ input.py
@@ -0,0 +1,8 @@
+print ("Bienvenido al programa")
+name = input("¿cual es tu nombre?")
+print ("Saludos " + name)
+
+#Suma de números
+primer_num = input("Inserte primer numero: ")
+segundo_num = input("Inserte segundo numero: ")
+print(int(primer_num) + int(segundo_num)) #Concatenar variables del mismo tipo
